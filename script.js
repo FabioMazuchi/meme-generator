a88b7914848cb66934dafb9c2456b3a09e29d113
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2021-11-02 11:08:32 -0300

    adiciona requisitos

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
 const inputFile = document.querySelector('#meme-insert');
+const input = document.querySelector('#text-input');
+const p = document.querySelector('#meme-text');
+const pai = document.querySelector('#meme-image-container');
+const fireBtn = document.querySelector('#fire');
+const waterBtn = document.querySelector('#water');
+const earthBtn = document.querySelector('#earth');
+const memesDiv = document.querySelector('.memes');
+
+const loadFile = function (event) {
+  const image = document.getElementById('meme-image');
+  image.removeAttribute('src');
+  image.src = URL.createObjectURL(event.target.files[0]);
+};
 
 inputFile.addEventListener('onchange', function (event) {
-  var image = document.getElementById('output');
+  const image = document.getElementById('output');
   image.src = URL.createObjectURL(event.target.files[0]);
 });
 
 function trocaImagemMeme(e) {
-  var image = document.getElementById('meme-image');
-  let selecionado = e.target.getAttribute('src');
+  const image = document.getElementById('meme-image');
+  const selecionado = e.target.getAttribute('src');
 
   image.setAttribute('src', selecionado);
-  
 }
 
 window.onload = function () {
-  const input = document.querySelector('#text-input');
-  const p = document.querySelector('#meme-text');
-  const pai = document.querySelector('#meme-image-container');
-  const fireBtn = document.querySelector('#fire');
-  const waterBtn = document.querySelector('#water');
-  const earthBtn = document.querySelector('#earth');
-  const memesDiv = document.querySelector('.memes');
-
-  let memes = memesDiv.children;
-  for (let i = 0; i < memes.length; i++) {
+  const memes = memesDiv.children;
+  for (let i = 0; i < memes.length; i += 1) {
     memes[i].addEventListener('click', trocaImagemMeme);
   }
 
@@ -46,14 +50,7 @@ window.onload = function () {
   });
 
   input.addEventListener('keyup', function () {
-    let frase = input.value;
+    const frase = input.value;
     p.innerHTML = frase;
   });
-};
-
-var loadFile = function (event) {
-  var image = document.getElementById('meme-image');
-  image.removeAttribute('src');
-  image.src = URL.createObjectURL(event.target.files[0]);
-  
 };
